eed67351c91df9ea0e694e7c275b3cc4d0919190
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-12 06:08:54 +0000

    Add user routes and controller for CRUD operations

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./db/db.js";
 import { PORT, CORS_ORIGIN } from "./constants.js";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
 app.use(cors({ origin: CORS_ORIGIN, credentials: true }));
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.get("/", (req, res) => {
   res.send("Netflix Clone API created by Ashutosh Gaurav");
 });
+
+app.use("/api/user", userRoutes);
 
 connectDB()
   .then(() => {
